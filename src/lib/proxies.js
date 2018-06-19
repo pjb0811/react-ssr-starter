43e09c5f8c67b925af13281817061246980726a4
jbpark <jbpark@wemakeprice.com>
2018-06-19 12:04:31 +0900

    fixed untracked files

--- src/lib/proxies.js
+++
@@ -1,9 +0,0 @@
-const proxies = {
-  '/assist': {
-    target: 'http://assist-api-dev.wemakeprice.com:8080',
-    changeOrigin: true,
-    pathRewrite: { '^/assist': '' }
-  }
-};
-
-module.exports = proxies;
